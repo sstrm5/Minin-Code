8129f25daca6b2a5a807f6006e0f1eb58ec94f56
Mikhail Ovechkin <hatedyoufromhello@yandex.ru>
2024-11-29 10:35:00 +0400

    edit settings

--- core/api/v1/events/handlers_for_admin.py
+++ core/api/v1/events/handlers_for_admin.py
@@ -14,8 +14,8 @@ router = Router(tags=['Events for admins'])
 @router.get('/get_events', response=ApiResponse)
 def get_list_events(
     request: HttpRequest,
+    token: str = Header(alias='Auth-Token'),
 ):
-    token = request.META['HTTP_AUTH_TOKEN']
     use_case = GetEventsAdminUseCase(
         event_service=ORMEventsAdminService(),
         customer_service=ORMCustomerService(),
